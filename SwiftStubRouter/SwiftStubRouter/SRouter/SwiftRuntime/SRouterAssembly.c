973833c1a6f31778234448c89aefd9fae4142a48
jintao <>
2019-10-30 23:06:22 +0800

    add arm64

--- SwiftStubRouter/SwiftStubRouter/SRouter/SwiftRuntime/SRouterAssembly.c
+++ SwiftStubRouter/SwiftStubRouter/SRouter/SwiftRuntime/SRouterAssembly.c
@@ -10,14 +10,30 @@
 
 __attribute__ ((noinline))
 void saveSelfRegister(int64_t * _self) {
+    #if defined(__arm64__) || defined(__aarch64__)
+    __asm__ volatile (
+         "str x20, [x0]\n"
+    );
+    #elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7S__)
+
+    #elif defined(__x86_64__)
     __asm__ volatile (
        "movq %r13, (%rdi)\n"
     );
+    #endif
 }
 
 __attribute__ ((noinline))
 void setSelfRegister(void* _self) {
+    #if defined(__arm64__) || defined(__aarch64__)
     __asm__ volatile (
-       "movq (%rdi), %r13\n"
+       "ldr x20, [x0]\n"
     );
+    #elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7S__)
+
+    #elif defined(__x86_64__)
+    __asm__ volatile (
+          "movq (%rdi), %r13\n"
+    );
+    #endif
 }
