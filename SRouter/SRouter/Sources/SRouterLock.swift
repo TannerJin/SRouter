78b3128312622b665c9781341efa7d812bbf9171
jintao <>
2019-11-15 17:03:11 +0800

    优化spinLock

--- SRouter/SRouter/Sources/SRouterLock.swift
+++ SRouter/SRouter/Sources/SRouterLock.swift
@@ -21,30 +21,52 @@ class SpinLock {
 }
 
 class SRouterSpinLock {
+    private static var pre_qos_class_key: pthread_key_t = 0
     private var lock_value: Int32 = 0
     
     func lock() {
         // 单核：停止多线程调度(关中断)
-        // 多核：1.锁住总线 or 2.锁住缓存中的lock_value行
-        while !OSAtomicCompareAndSwap32(0, 1, &lock_value) {
-            
-        }
+        // 多核：1.锁住总线 2.锁住缓存的lock_value行
+        while !OSAtomicCompareAndSwap32(0, 1, &lock_value) {}
         
         // QOS_CLASS_BACKGROUND        : 9
         // QOS_CLASS_UTILITY           : 17
         // QOS_CLASS_DEFAULT           : 21
         // QOS_CLASS_USER_INITIATED    : 25
         // QOS_CLASS_USER_INTERACTIVE  : 33
-        // 优先级翻转问题；最后在unlock的时候，恢复线程优先级；可以通过pthread_setspecific存储和设置
-        if qos_class_self().rawValue <= QOS_CLASS_BACKGROUND.rawValue {
-            pthread_set_qos_class_self_np(QOS_CLASS_UTILITY, 0)
+
+        // 优先级反转:
+        // lock   提升优先级
+        // unlock 恢复线程优先级
+        let pre_qos_class_self = qos_class_self()
+        
+        if pre_qos_class_self.rawValue <= QOS_CLASS_BACKGROUND.rawValue {
+            
+            let pointer = malloc(MemoryLayout<qos_class_t>.size)
+            pointer?.bindMemory(to: qos_class_t.self, capacity: 1).initialize(to: pre_qos_class_self)
+            
+            if SRouterSpinLock.pre_qos_class_key == 0 {
+                pthread_key_create(&SRouterSpinLock.pre_qos_class_key, nil)
+            }
+            pthread_setspecific(SRouterSpinLock.pre_qos_class_key, pointer)
+            
+            pthread_set_qos_class_self_np(QOS_CLASS_DEFAULT, 0)
         }
     }
     
     func unlock() {
-        // 结果储存在当前线程的rax寄存器中，if判断没有多线程问题
-        if OSAtomicDecrement32(&lock_value) != 0 {
-           abort()
+        var pre_qos_class: UnsafeMutablePointer<qos_class_t>?
+
+        if SRouterSpinLock.pre_qos_class_key != 0 {
+            pre_qos_class = pthread_getspecific(SRouterSpinLock.pre_qos_class_key)?.assumingMemoryBound(to: qos_class_t.self)
+            pthread_setspecific(SRouterSpinLock.pre_qos_class_key, nil)
+        }
+        
+        OSAtomicCompareAndSwap32(1, 0, &lock_value)
+        
+        if pre_qos_class != nil {
+            pthread_set_qos_class_self_np(pre_qos_class!.pointee, 0)
+            free(pre_qos_class)
         }
     }
 }
